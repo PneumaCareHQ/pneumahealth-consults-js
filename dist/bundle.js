f6dbc106f0a463314dfe3916dbc2ec6f5658f04a
Abayomi Ayodele <samabayomi301@gmail.com>
2022-07-30 16:17:17 +0100

    update bundle.js

--- dist/bundle.js
+++ dist/bundle.js
@@ -67,7 +67,6 @@
       e.exports = n(593);
     },
     593: function (e, m, n) {
-      "use strict";
       function t(e, m) {
         var n = Object.keys(e);
         if (Object.getOwnPropertySymbols) {
